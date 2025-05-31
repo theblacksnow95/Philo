861bb08473795245ceb8381f2a25e6c68a6c996a
theblacksnow95 <enrique.murillo24@gmail.com>
2025-05-31 15:57:19 +0200

    all

--- thread_test.c
+++ thread_test.c
@@ -6,7 +6,7 @@
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 15:44:22 by emurillo          #+#    #+#             */
-/*   Updated: 2025/05/30 18:18:08 by emurillo         ###   ########.fr       */
+/*   Updated: 2025/05/31 15:12:36 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,54 @@
 // The thread if not sleep will take longer than the main to finish, so
 // using usleep will wait for it to finish, same that the pthread_join func
 
+int	n;
+
 void	*test(void *arg)
 {
-	int	i;
-	int	n;
+	int			i;
+	int			n;
+	pthread_t	thread_id;
 
-	i = 0;
+	thread_id = pthread_self();
+	i = 1;
 	n = *(int *) arg;
+	printf("Initiating thread ID = {%lu}\n", thread_id);
 	while (i < n)
 	{
-		printf("Test #%d\n", i + 1);
+		printf("Test #%d\n", i);
 		i++;
+		usleep(50);
 	}
+	return (NULL);
+}
+
+void	*change_n(void *arg)
+{
+	int	n;
+
+	n = *(int *) arg;
+	if (n > 2)
+		n = n + 50;
+	printf("Value of n modified.\n");
 	return (NULL);
 }
 
 int	main(int ac, char **av)
 {
-	int			n;
 	pthread_t	thread1;
+	pthread_t	thread2;
 
 	if (ac != 2 || !av[1])
 		return (1);
 	n = atoi(av[1]);
+	if (n < 2)
+		return (1);
+	printf("Value of n before: %d\n", n);
 	pthread_create(&thread1, NULL, test, (void *)&n);
-	usleep(80);
-	pthread_cancel(thread1);
+	pthread_create(&thread2, NULL, change_n, (void *)&n);
+	usleep(1000);
+	// pthread_cancel(thread1);
 	pthread_join(thread1, NULL);
+	printf("Value of n after: %d\n", n);
 	return (0);
 }
